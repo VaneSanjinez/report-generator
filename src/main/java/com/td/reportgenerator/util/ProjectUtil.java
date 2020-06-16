fdbb43d8544882f78f564bacd9e084d4484c7a1c
VaneSanjinez <vane.sanji@gmail.com>
2020-06-15 23:38:32 -0400

    Add name, id and description to get of json response from gitlab and github WIP for bitbucket

--- src/main/java/com/td/reportgenerator/util/ProjectUtil.java
+++ src/main/java/com/td/reportgenerator/util/ProjectUtil.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.td.reportgenerator.model.Project;
-import jdk.nashorn.internal.ir.ObjectNode;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +16,34 @@ public class ProjectUtil {
     public ResponseEntity<Project[]> parseToProject(ResponseEntity<?> allProjects) {
         System.out.println("from project util");
         Object[] projectList = (Object[]) allProjects.getBody();
-        System.out.println(projectList);
-
-//        for (int i = 0; i < projectList.length; i ++){
-////            projectList[i].to
-//
-////                System.out.println(projectList[i]);
-//            System.out.println(projectList[i].getClass().getDeclaredFields().toString());
-//        }
-
-        int i = 0;
-        for (Object project: projectList){
-            System.out.println(project);
+        System.out.println(projectList.length);
+        JSONArray projectArray = new JSONArray();
+        for (Object project: projectList) {
+//            System.out.println(project);
+            Project projectObject = new Project();
             ObjectMapper mapper = new ObjectMapper();
             try {
                 String jsonProject = mapper.writeValueAsString(project);
                 JsonNode node = new ObjectMapper().readTree(jsonProject);
-//                JSONObject node =   new JSONObject(jsonProject);
-                if(node.has("description")){
-//                    System.out.println(jsonProject);
-//                    System.out.println(node.getString("description"));
+                if (node.has("id")) {
+                    projectObject.setId(node.get("id").toString());
+                    System.out.println(projectObject.getId());
+//                    System.out.println("has id!");
+                } if (node.has("name")) {
+//                    if (node.has("name") || node.path("values").has("name")) {
+//                    if (node.has("name")) {
+                        System.out.println("has name!");
+//                        if(node.has("name")){
+                            projectObject.setName(node.get("name").toString());
+//                        }else{
+//                            projectObject.setName(node.path("values").path("name").toString());
+//                        }
+                        System.out.println(projectObject.getName());
+//                }
+                }if (node.has("description") && !node.get("description").toString().isEmpty()) {
                     System.out.println("has description!");
-                    if(node.get("description").asText() != null || node.get("description").asText() != ""){
-                        System.out.println(node.get("description").asText());
-                        System.out.println(node.get("description").textValue());
-                    }
-//                    System.out.println(node.get("description").asText());
-//                    System.out.println(node.get("description").textValue());
+                    projectObject.setName(node.get("description").toString());
+                    System.out.println(projectObject.getDescription());
                 }
 //                System.out.println("at try part");
 //                System.out.println(jsonProject);
