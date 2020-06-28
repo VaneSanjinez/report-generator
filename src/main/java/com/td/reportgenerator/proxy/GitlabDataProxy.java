910f1aed523c1710859da811096f8c2e790e1f7b
VaneSanjinez <vane.sanji@gmail.com>
2020-06-27 23:45:28 -0400

    Modify access to private token from proxy base, change return of commit function

--- src/main/java/com/td/reportgenerator/proxy/GitlabDataProxy.java
+++ src/main/java/com/td/reportgenerator/proxy/GitlabDataProxy.java
@@ -12,14 +12,14 @@ public class GitlabDataProxy extends GitlabBaseProxy{
     @Autowired
     GitlabUtil gitlabUtil;
 
-    String privateToken = personalToken;
+    private String privateToken = null;
     RestTemplate restTemplate = new RestTemplate();
 
     //Projects
     public ResponseEntity<Object []> getAllProjects(){
         //http://localhost:9090/api/projects
         String url = GITLAB_BASE_URL + "projects";
-        HttpEntity request = gitlabUtil.declareTemplate(privateToken);
+        HttpEntity request = gitlabUtil.declareTemplate(this.personalToken);
         ResponseEntity<Object[]> allProjects = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
@@ -33,7 +33,7 @@ public class GitlabDataProxy extends GitlabBaseProxy{
     public ResponseEntity<Object> getProjectById(String projectID) {
 //        http://localhost:9090/api/projects/18625237
         String url = GITLAB_BASE_URL + "projects/" + projectID;
-        HttpEntity request = gitlabUtil.declareTemplate(privateToken);
+        HttpEntity request = gitlabUtil.declareTemplate(this.personalToken);
         ResponseEntity<Object> project = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
@@ -46,7 +46,7 @@ public class GitlabDataProxy extends GitlabBaseProxy{
     public ResponseEntity<Object[]> getProjectsByUserId(String userId) {
 //        http://localhost:9090/api/projects/users/vane-sanjinez?private_token=cxXdxSAm8KmZZe7RZ7i6
         String url = GITLAB_BASE_URL + "projects/users/" + userId;
-        HttpEntity request = gitlabUtil.declareTemplate(privateToken);
+        HttpEntity request = gitlabUtil.declareTemplate(this.personalToken);
         ResponseEntity<Object[]> projectsByUserId = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
@@ -60,7 +60,7 @@ public class GitlabDataProxy extends GitlabBaseProxy{
     public ResponseEntity<Object[]> getAllCommitsByProjectId(String projectId){
 //        http://localhost:9090/api/commits/{projectId}
         String url = GITLAB_BASE_URL + "commits/" + projectId;
-        HttpEntity request = gitlabUtil.declareTemplate(privateToken);
+        HttpEntity request = gitlabUtil.declareTemplate(this.personalToken);
         ResponseEntity<Object[]> commitsByProjectId = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
@@ -69,6 +69,6 @@ public class GitlabDataProxy extends GitlabBaseProxy{
         );
 
         System.out.println(commitsByProjectId.getBody());
-        return null;
+        return commitsByProjectId;
     }
 }
