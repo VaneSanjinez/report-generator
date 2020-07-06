ac6ec461ab7a4122181cf15c358c9384d2c7a218
VaneSanjinez <vane.sanji@gmail.com>
2020-07-05 22:23:53 -0400

    Refactor code, delete commented lines unused

--- src/main/java/com/td/reportgenerator/util/CommitUtil.java
+++ src/main/java/com/td/reportgenerator/util/CommitUtil.java
@@ -43,42 +43,29 @@ public class CommitUtil {
         return commitObject;
     }
 
-//    public Commit jsonToCommit(ResponseEntity<Object> commitResponse)  {
-//        Object commitResponseBody = commitResponse.getBody();
-//        JSONObject jsonCommit = null;
-//        try {
-//            jsonCommit = objectToJSONObject(commitResponseBody);
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
-//        Commit commit = jsonObjectToCommit(jsonCommit);
-//        return commit;
-//    }
-
     private JSONObject objectToJSONObject(Object commitObject) throws JsonProcessingException { //Parse from object to JSON Object
         ObjectMapper mapper = new ObjectMapper();
         String jsonProject = mapper.writeValueAsString(commitObject);
         JsonNode node = new ObjectMapper().readTree(jsonProject);
         JSONObject json = new JSONObject();
 
-        json.put("id", commitId(node));
+        json.put("id", getCommitId(node));
 
-        if(node.has("commit")){ //github parse
+        if(node.has("commit")){ //Parse to github
             JsonNode child = node.get("commit");
             JsonNode author = child.get("author");
             json.put("authorName", author.get("name").asText());
             json.put("authorEmail", author.get("email").asText());
-            json.put("creationDate",convert(author.get("date").asText()));
+            json.put("creationDate",convertToDate(author.get("date").asText()));
             json.put("message", child.get("message").asText());
             json.put("webUrl", child.get("url").asText());
 
         }
 
-        //parse to gitlab commit
-
+        //Parse to gitlab commit
         json.put("authorName", node.get("author_name").asText());
         json.put("authorEmail", node.get("author_email").asText());
-        json.put("creationDate",convert(node.get("created_at").asText()));
+        json.put("creationDate",convertToDate((node.get("created_at").asText())));
         json.put("message", node.get("message").asText());
         json.put("webUrl", node.get("web_url").asText());
         return json;
@@ -95,13 +82,13 @@ public class CommitUtil {
         return commit;
     }
 
-    private DateTime convert(String dateString) {//Use of DateTime from JodaTime library
+    private DateTime convertToDate(String dateString) {//Use of DateTime from JodaTime library
         DateTime date;
         date = new DateTime(dateString);
         return date;
     }
 
-    private String commitId(JsonNode bodyResponse){
+    private String getCommitId(JsonNode bodyResponse){
         String commitId = bodyResponse.has("id") ? bodyResponse.get("id").asText() :
                           bodyResponse.has("sha")? bodyResponse.get("sha").asText(): null;
         return commitId;
