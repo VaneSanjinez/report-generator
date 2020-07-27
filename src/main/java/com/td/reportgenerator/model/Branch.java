3bb5a09eec5798de6b93d08fc8bfe4f48a869d34
Vanessa Sanjinez <vane.sanji@gmail.com>
2020-07-26 22:05:06 -0400

    Creation of Branch model WIP with possible changes at main fields

---
+++ src/main/java/com/td/reportgenerator/model/Branch.java
@@ -0,0 +1,37 @@
+package com.td.reportgenerator.model;
+
+public class Branch {
+    public String name;
+    public boolean merged;
+    public String webUrl;
+
+    public Branch(String name, boolean merged, String webUrl) {
+        this.name = name;
+        this.merged = merged;
+        this.webUrl = webUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMerged() {
+        return merged;
+    }
+
+    public void setMerged(boolean merged) {
+        this.merged = merged;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+}
